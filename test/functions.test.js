737a26c8ce7979b65fff26aa24220fe1aac53ced
JeffAllison <itsjallison@gmail.com>
2022-03-10 12:10:17 -0800

    passing test for multiplyBySeven

--- test/functions.test.js
+++ test/functions.test.js
@@ -2,6 +2,7 @@
 import {
     addExclamationPoints,
     multiplyBySeven,
+    multiplyBy12ThenHalve,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
@@ -12,15 +13,52 @@ test('addExclamationPoints', (expect) => {
     const actual = addExclamationPoints('bunny rabbit', '!!!');
 
     expect.equal(actual, expected, 'bunny rabbit should appear as bunny rabbit!!!');
+
+    const expected2 = 'green!!!';
+
+    const actual2 = addExclamationPoints('green', '!!!');
+
+    expect.equal(actual2, expected2, 'green should appear as green!!!');
+
+    const expected3 = 'yup!!!';
+
+    const actual3 = addExclamationPoints('yup', '!!!');
+
+    expect.equal(actual3, expected3, 'yup should appear as yup!!!');
 });
 
-test('mutiplybySeven', (expect) => {
+test('multiplyBySeven', (expect) => {
     const expected = 28;
 
     const actual = multiplyBySeven(4, 7);
 
-    expect.equal(actual, expected);
+    expect.equal(actual, expected, '4 times 7 is 28');
+
+    const expected2 = 56;
+
+    const actual2 = multiplyBySeven(8, 7);
+
+    expect.equal(actual2, expected2, '8 times 7 is 56');
+
+    const expected3 = 14;
+
+    const actual3 = multiplyBySeven(2, 7);
+
+    expect.equal(actual3, expected3, '2 times 7 is 14');
 });
+
+test('multiplyBy12ThenHalve', (expect) => {
+    const expected = 28;
+
+    const actual = multiplyBySeven(4, 7);
+
+    expect.equal(actual, expected, '4 times 7 is 28');
+});
+
+
+
+
+
 
 skip('this test should be skipped', (expect) => {
     const expected = true;
