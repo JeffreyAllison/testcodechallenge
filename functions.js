999d59d177b70a236f5c0f5946e777428e323964
JeffAllison <itsjallison@gmail.com>
2022-03-10 15:31:41 -0800

    passing test for getSecondItem

--- functions.js
+++ functions.js
@@ -63,7 +63,7 @@ Output:
 */
 
 export function divideThenMultiply(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    return firstNumber / secondNumber * thirdNumber;
 }
 
 
@@ -80,7 +80,7 @@ Output:
 */
 
 export function returnAsAnArray(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    return [firstNumber, secondNumber, thirdNumber];
 }
 
 // This function should take in three numbers and return those numbers mushed together as a string
@@ -96,7 +96,7 @@ Output:
 */
 
 export function returnAsAString(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    return [firstNumber] + [secondNumber] + [thirdNumber];
 }
 
 // This function should take in two numbers and return a greeting announcing that the sum of those numbers is today's lucky number
@@ -112,7 +112,7 @@ Output:
 */
 
 export function makeLuckyGreeting(firstNumber, secondNumber) {
-    return true;
+    return 'Your lucky number for the day is ' + String(firstNumber + secondNumber);
 }
 
 // This function should take an array and return the second item in the array
@@ -129,7 +129,7 @@ Output:
 */
 
 export function getSecondItem(array) {
-    return true;
+    return array[1];
 }
 
 // This function should take an array and return the LAST item in the array, no matter the array's length
@@ -156,5 +156,5 @@ export function getLastItem(array) {
 // Ask yourself: even if you cannot predict the result exactly, what _do_ you know about what it should return? Use that expectation to write your test
 
 export function getRandomNumber() {
-    return true;
+    return trye;
 }
--- test/functions.test.js
+++ test/functions.test.js
@@ -3,6 +3,13 @@ import {
     addExclamationPoints,
     multiplyBySeven,
     multiplyBy12ThenHalve,
+    divideThenMultiply,
+    returnAsAnArray,
+    returnAsAString,
+    makeLuckyGreeting,
+    getSecondItem,
+    //getLastItem,
+    //getRandomNumber,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
@@ -48,16 +55,131 @@ test('multiplyBySeven', (expect) => {
 });
 
 test('multiplyBy12ThenHalve', (expect) => {
-    const expected = 28;
+    const expected = 6;
 
-    const actual = multiplyBySeven(4, 7);
+    const actual = multiplyBy12ThenHalve(1, 12, 2);
 
-    expect.equal(actual, expected, '4 times 7 is 28');
+    expect.equal(actual, expected, '1 times 12 divided by 2 is 6');
+
+    const expected2 = 612;
+
+    const actual2 = multiplyBy12ThenHalve(102, 12, 2);
+
+    expect.equal(actual2, expected2, '102 times 12 divided by 2 is 612');
+
+    const expected3 = 27270;
+
+    const actual3 = multiplyBy12ThenHalve(4545, 12, 2);
+
+    expect.equal(actual3, expected3, '4545 times 12 divided by 2 is 27270');
 });
 
+test('divideThenMultiply', (expect) => {
+    const expected = 30;
+
+    const actual = divideThenMultiply(12, 2, 5);
+
+    expect.equal(actual, expected, '12 divided by 2 times 5 is 30');
+
+    const expected2 = 9000000;
+
+    const actual2 = divideThenMultiply(4500, 500, 1000000);
+
+    expect.equal(actual2, expected2, '4500 divided by 500 times 1000000 is 9000000');
+
+    const expected3 = 441;
+
+    const actual3 = divideThenMultiply(42, 2, 21);
+
+    expect.equal(actual3, expected3, '42 divided by 2 times 21 is 441');
+});
+
+test('returnAsAnArray', (expect) => {
+    const expected = [1, 2, 3];
+
+    const actual = returnAsAnArray(1, 2, 3);
+
+    expect.deepEqual(actual, expected, '1, 2, 3 is displayed as an array');
+
+    const expected2 = [4, 5, 6];
+
+    const actual2 = returnAsAnArray(4, 5, 6);
+
+    expect.deepEqual(actual2, expected2, '4, 5, 6 is displayed as an array');
+
+    const expected3 = [7, 8, 9];
+
+    const actual3 = returnAsAnArray(7, 8, 9);
+
+    expect.deepEqual(actual3, expected3, '7, 8, 9 is displayed as an array');
+
+});
+
+test('returnAsAString', (expect) => {
+    const expected = (145);
+
+    const actual = returnAsAString([1] + [4] + [5]);
+
+    expect.equal(actual, expected, '145 is a string because inputs are [1]+[4]+[5}');
+
+    const expected2 = (256);
+
+    const actual2 = returnAsAString([2] + [5] + [6]);
+
+    expect.equal(actual2, expected2, '256 is a string because inputs are [2]+[5]+[6}');
+
+    const expected3 = (367);
+
+    const actual3 = returnAsAString([3] + [6] + [7]);
+
+    expect.equal(actual3, expected3, '367 is a string because inputs are [3]+[6]+[7}');
+});
+
+test('makeLuckyGreeting', (expect) => {
+    const expected = ('Your lucky number for the day is 12');
+
+    const actual = makeLuckyGreeting(5, 7);
+
+    expect.equal(actual, expected, 'message + 12 is because number1 and number2 are generated as a String');
+
+    const expected2 = ('Your lucky number for the day is 16');
+
+    const actual2 = makeLuckyGreeting(8, 8);
+
+    expect.equal(actual2, expected2, 'message + 16 is because number1 and number2 are generated as a String');
+
+    const expected3 = ('Your lucky number for the day is 15');
+
+    const actual3 = makeLuckyGreeting(5, 10);
+
+    expect.equal(actual3, expected3, 'message + 12 is because number1 and number2 are generated as a String');
 
 
+});
 
+test('getSecondItem', (expect) => {
+    const expected = 'cats';
+
+    const actual = getSecondItem(['dogs', 'cats', 'ants', 'worms']);
+
+    expect.equal(actual, expected, 'cats');
+});
+
+test('getLastItem', (expect) => {
+    const expected = ();
+
+    const actual = getLastItem();
+
+    expect.equal(actual, expected, '');
+});
+
+//test('getRandomNumber', (expect) => {
+//const expected = ();
+
+//const actual = getRandomNumber();
+
+//expect.equal(actual, expected, '');
+//});
 
 
 skip('this test should be skipped', (expect) => {
